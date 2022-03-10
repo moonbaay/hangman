cf2967cfe4e52afd8f55e20409b85b4ae001b2c5
Lu Thao <98998807+moonbaay@users.noreply.github.com>
2022-03-10 16:21:43 -0600

    init commit

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const showLife = document.querySelector(".life");
+var alertMessage = document.querySelector(".alert");
+const newGame = document.querySelector(".new-game-btn");
+var keyboards = document.querySelector('.keyboard');
+var blank = document.querySelector(".blank");
+var currentWord = null;
+let life = 6;
+var guessList = [];
+var word = "";
+
 // ----------------
 // get random word
 //-----------------
@@ -22,184 +32,154 @@ function getWord() {
   let randomIndex = Math.floor(Math.random() * words.length);
 
   var randomWord = words[randomIndex];
-  return randomWord;
+  word = randomWord;
+}
+
+// ----------------
+// create keyboard
+//-----------------
+
+function getLetterBtn() {
+  let letterBtnHTML = "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map(
+      (letter) =>
+      `
+       <button class="letter-btn" id="` +letter +`" onClick="chosenLetter('`+letter+`')">` +letter +`</button>
+      `
+    )
+    .join("");
+
+  keyboards.innerHTML = letterBtnHTML;
 }
 
 // ----------------
 // print character and blank lines
 //-----------------
 
-function printWordState(answer, list) {
-
-  var status = null;
-  var blank = document.querySelector('.blank');
-  console.log(answer);
-  status = answer.split('').map(letter =>(list.indexOf(letter)>= 0 ? letter : "_")).join('');
-  blank.innerHTML = status;
-  return status.includes(answer);
+function printWordState() {
+  
+  console.log(word);
+  currentWord = word
+    .split("")
+    .map((letter) => (guessList.indexOf(letter) >= 0 ? letter : "_"))
+    .join("");
+  blank.innerHTML = currentWord;
+  
 }
 
 // ----------------
+// get guess
+//-----------------
+function chosenLetter(guess) {
+  guessList.indexOf(guess) === -1 ? guessList.push(guess) : null;
+  document.getElementById(guess).setAttribute("disabled", true);
+  document.getElementById(guess).setAttribute("style", "color:lightgrey;");
+
+  if (word.indexOf(guess) >= 0) {
+    printWordState();
+    ifGameWon();
+  } else if (word.indexOf(guess) === -1) {
+    life--;
+    updateLife();
+    ifGameLost();
+    displayDraw(life);
+  }
+}
+
+// ----------------
+// check if won
+//-----------------
+function ifGameWon() {
+  if ((currentWord === word)) {
+
+    // alertMessage.textContent = `You Won !!! the answer is : ${word}`
+    displayAlert('you won','completed');
+  }
+}
+
+// ----------------
+// check if lost
+//-----------------
+function ifGameLost(){
+  if(life === 0){
+    blank.textContent = `Answer is: ${word}`
+    displayAlert('you lose','incorrect');
+  }
+}
+
+// ----------------
+// update life
+//-----------------
+function updateLife(){
+  showLife.textContent = `Life: ${life}`;
+}
+// ----------------
 // create hangman display
 //-----------------
-
+var leftLeg = document.querySelector(".left-leg");
+var rightLeg = document.querySelector(".right-leg");
+var rightArm = document.querySelector('.right-arm');
+var leftArm = document.querySelector(".left-arm")
+var centerBody = document.querySelector('.center-body')
+var head = document.querySelector(".head");
 function displayDraw(life) {
-  const manContainer = document.querySelector(".man-container");
+  
   switch (life) {
     case 0:
-      console.log("-------------");
-      console.log("|          |");
-      console.log("|          O");
-      console.log("|         /|\\");
-      console.log("|         / \\");
-      console.log("|");
+      leftLeg.style.visibility = 'visible';
       break;
     case 1:
-      console.log("-------------");
-      console.log("|          |");
-      console.log("|          O");
-      console.log("|         /|\\");
-      console.log("|         / ");
-      console.log("|");
+      rightLeg.style.visibility='visible';
       break;
-
     case 2:
-      console.log("-------------");
-      console.log("|          |");
-      console.log("|          O");
-      console.log("|         /|\\");
-      console.log("|         ");
-      console.log("|");
+      leftArm.style.visibility = "visible";
       break;
-
     case 3:
-      console.log("-------------");
-      console.log("|          |");
-      console.log("|          O");
-      console.log("|         /|");
-      console.log("|         ");
-      console.log("|");
+      rightArm.style.visibility = 'visible';
       break;
     case 4:
-      console.log("-------------");
-      console.log("|          |");
-      console.log("|          O");
-      console.log("|         /");
-      console.log("|         ");
-      console.log("|");
+      centerBody.style.visibility= 'visible'
       break;
-
     case 5:
-      console.log("-------------");
-      console.log("|          |");
-      console.log("|          O");
-      console.log("|         ");
-      console.log("|         ");
-      console.log("|");
-      
-      // const head = document.querySelector(".head");
-      // var after = head.querySelector('::after');
-      // after.style.visibility = "visible";
+      head.style.visibility = "visible";
       break;
-    // case 6:
-    //   console.log("-------------");
-    //   console.log("|          |");
-    //   console.log("|          ");
-    //   console.log("|         ");
-    //   console.log("|         ");
-    //   console.log("|");
-      
-    //   break;
     default:
       break;
   }
 }
 
-let life = 6;
-var guessList = [];
-var word = getWord();
-
-const showLife = document.querySelector('.life');
-const alertMessage = document.querySelector(".alert");
-const newGame  = document.querySelector('.new-game-btn');
-
-// ----------------
-// create keyboard
-//-----------------
-
-function getLetterBtn(){
-  let letterBtnHTML = "abcdefghijklmnopqrstuvwxyz".split('').map((letter)=>
-  `
-    <button class="letter-btn" id="`+letter+`">`+letter+`</button>
-  `).join("");
-
-  document.querySelector('.keyboard').innerHTML = letterBtnHTML;
-
-  
-}
-getLetterBtn();
-
-const inputLetter = document.querySelectorAll(".letter-btn");
-
-// ----------------
-// get user input guess character
-//-----------------
-function getGuess(answer, list) {
-
-  inputLetter.forEach((item) => {
-
-    item.addEventListener("click", function () {
-
-      console.log(item.outerText);
-      list.push(item.outerText.charAt(0));
-      printWordState(answer,list);
-      
-      if(printWordState(answer,list)){
-        displayAlert(`Congratulation you complete the word: ${word}`,`completed`)
-      }
-       if(!answer.includes(item.outerText)){
-        displayAlert(`oops try again`, `incorrect`);
-        setTimeout(() => {
-            alertMessage.textContent = ""
-            alertMessage.classList.remove(`alert-incorrect`);
-        }, 3000);  
-       
-            life--;
-          displayDraw(life);
-        }
-      showLife.textContent = `Life: ${life}`;
-     
-    });
-
-    
-  });
-
-}
-
 // ----------------
 // display alert
 //-----------------
-function displayAlert(text,action){
-
-    alertMessage.textContent = text;
-    alertMessage.classList.add(`alert-${action}`);
-    //remove alert
-    // setTimeout(() => {
-
-    //   alertMessage.textContent = ""
-    //   alertMessage.classList.remove(`alert-${action}`);
-      
-    // }, 3000);
+function displayAlert(text, action) {
+  alertMessage.textContent = text;
+  alertMessage.classList.add(`alert-${action}`);
 }
 
+const inputLetter = document.querySelectorAll(".letter-btn");
 
-newGame.addEventListener('click',function(){
-  word =getWord();
-  getGuess(word,guessList)
-  printWordState(word,guessList);
+function reset() {
+  getWord();
+  guessList = [];
+  printWordState();
+  getLetterBtn();  
+  updateLife();
   life = 6;
-})
+  showLife.textContent = `Life: ${life}`;
+  alertMessage.textContent = "";
+  displayDraw(life);
+  leftLeg.style.visibility = 'hidden';
+  rightLeg.style.visibility = "hidden";
+  leftArm.style.visibility = "hidden";
+  rightArm.style.visibility = "hidden";
+  centerBody.style.visibility = "hidden";
+  head.style.visibility = "hidden";
+}
+newGame.addEventListener('click',reset);
+getWord();
+getLetterBtn();
+printWordState();
 
 
 
- 
